03b03a512496caf0a5cad1f308ad07e7e139bf79
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2021-12-21 01:20:34 +0100

    rephrased the tests

--- testing-enzyme/src/components/Counter.test.js
+++ testing-enzyme/src/components/Counter.test.js
@@ -7,25 +7,25 @@ import Button from "./Button";
 
 
 describe("Counter component", () => {
-  it("should component render correctly", () => {
+  it("should render correctly", () => {
     const CounterComponent = renderer.create(<Counter />).toJSON();
     expect(CounterComponent).toMatchSnapshot();
   });
 
-  it("should the initial state of the Counter be 0 if start is not defined", () => {
+  it("should have the initial state of Counter == 0 if start is not defined", () => {
     const wrapper = shallow(<Counter />);
     const counterValue = wrapper.find(".counter-value").text();
     expect(counterValue).toEqual("0");
   });
 
-  it("should the initial state of the Counter be 10 if start = 10", () => {
+  it("should have the initial state of the Counter == 10 if start == 10", () => {
     const wrapper = mount(<Counter start="10" />);
     expect(wrapper.prop('start')).toEqual("10");
     const counterValue = wrapper.find(".counter-value").text();
     expect(counterValue).toEqual("10");
   });
 
-  it("should the component have buttons to increment and decrement the counter", () => {
+  it("should have buttons to increment and decrement the counter", () => {
     const wrapper = shallow(<Counter />);
     expect(wrapper.containsAllMatchingElements([
       <Button label="+" />,
@@ -33,7 +33,7 @@ describe("Counter component", () => {
     ])).toEqual(true);
   });
 
-  it("should the counter value decrement when the decrement button is clicked", () => {
+  it("should decrement the counter value when the decrement button is clicked", () => {
     const wrapper = mount(<Counter />);
     const decrementButton = wrapper.find(".decrement-btn");
     let counterValue = wrapper.find(".counter-value").text();
@@ -43,7 +43,7 @@ describe("Counter component", () => {
     expect(counterValue).toEqual("-1");
   });
 
-  it("should the counter value increment when the increment button is clicked", () => {
+  it("should increment the counter value when the increment button is clicked", () => {
     const wrapper = mount(<Counter />);
     const incrementButton = wrapper.find(".increment-btn");
     let counterValue = wrapper.find(".counter-value").text();
@@ -53,7 +53,7 @@ describe("Counter component", () => {
     expect(counterValue).toEqual("1");
   });
 
-  it("should the counter state change when the change button is clicked", () => {
+  it("should change the counter state when the change button is clicked", () => {
     // value of the input field is not the value of the counter
     const wrapper = mount(<Counter />);
 
@@ -74,7 +74,7 @@ describe("Counter component", () => {
     expect(counterValue).toEqual("20");
   });
 
-  it("should the counter value == start after reset button is clicked", () => {
+  it("should have the counter value == start after reset button is clicked", () => {
     const wrapper = mount(<Counter start="10" />);
     let counterValue = wrapper.find(".counter-value").text();
     const resetButton = wrapper.find(".reset-btn");
